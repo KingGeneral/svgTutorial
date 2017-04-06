7e811d3433dba3b5067d194be23f66f7166ffacc
KingGeneral <zone.nano09@gmail.com>
2017-04-06 13:29:46 +0700

    fixing crocodile, added blackpaw v1
    blackpaw.v2 (development)

---
+++ demo/scripts/custom.blackpaw.v2.snap.svg.js
@@ -0,0 +1,142 @@
+// kinggeneral
+// 
+// crocodile svg test
+// with  snap svg 0.5.1
+// 
+// var s  = Snap("#svg");
+
+//first setup the svg in <svg>
+//then apply this move
+window.onload = function () {
+	//initialize
+	var blackpaw  = Snap.select("#blackpaw"),
+        pawCenter = blackpaw.select("#pawCenter"),
+        paw1      = blackpaw.select("#paw1"),
+        paw2      = blackpaw.select("#paw2"),
+        paw3      = blackpaw.select("#paw3"),
+        paw4      = blackpaw.select("#paw4"),
+        timer;
+
+    // var croc = Snap.select("#croc") ,	//get id
+    //     head = croc.select("#upper-head"),	//get id
+    //     jaw = croc.select("#upper-jaw"),	//get id
+    //     symbol = croc.select("#symbol"),	//get id
+    //     timer;								//set timer variable for animation
+
+    // central point for spin
+    // left,top
+    var pivots = [
+                [44, 147],		//pivot [0]
+                [92, 126]		//pivot [1]
+            ];
+    // paw1.attr({ stroke: 'red', strokeDasharray: "40 280", fill: 'none', strokeWidth: 20 });
+    // paw1.transform('r0,100,100');
+    // paw1.animate({ transform: "r360,100,100" }, 1000, mina.linear, anim);
+    
+    //set test
+    // paw1.attr({
+    //     fill: "none",
+    //     stroke: 'red',
+    //     strokeWidth: 30,
+    //     strokeDasharray: "10 300 40 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10",
+    //     strokeDashoffset: 50
+    // });
+    // paw2.attr({
+    //     fill: "none",
+    //     stroke: 'red',
+    //     strokeWidth: 30,
+    //     strokeDasharray: "10 300 40 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10",
+    //     strokeDashoffset: 50
+    // });
+    // paw3.attr({
+    //     fill: "none",
+    //     stroke: 'red',
+    //     strokeWidth: 30,
+    //     strokeDasharray: "10 300 40 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10",
+    //     strokeDashoffset: 50
+    // });
+    // paw4.attr({
+    //     fill: "none",
+    //     stroke: 'red',
+    //     strokeWidth: 30,
+    //     strokeDasharray: "10 300 40 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10",
+    //     strokeDashoffset: 50
+    // });
+    // pawCenter.attr({
+    //     fill: "none",
+    //     stroke: 'red',
+    //     strokeWidth: 30,
+    //     strokeDasharray: "10 300 40 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10",
+    //     strokeDashoffset: 50
+    // });
+
+    function close() {
+        clearTimeout(timer);
+
+        Snap.animate(0,400, function( value ){
+           pawCenter.attr({ 'strokeDashoffset': value })
+           paw1.attr({ 'strokeDashoffset': value })
+           paw2.attr({ 'strokeDashoffset': value })
+           paw3.attr({ 'strokeDashoffset': value })
+           paw4.attr({ 'strokeDashoffset': value })
+        },5000 );
+
+        // paw1.animate({ 
+        //     transform: "r" + [8, pivots[0]]
+        // }, 500, mina.backin);
+        // paw1.transform('r0,100,100');
+
+        // paw2.animate({ 
+        //     transform: "r" + [8, pivots[0]]
+        // }, 500, mina.backin);
+
+        // paw3.animate({ 
+        //     transform: "r" + [8, pivots[0]]
+        // }, 500, mina.backin);
+
+        // paw4.animate({ 
+        //     transform: "r" + [8, pivots[0]]
+        // }, 500, mina.backin);
+        
+        // jaw.animate({
+        //     transform: "r" + [37, pivots[1]]
+        // }, 500, mina.backin);
+
+        // timer = setTimeout(function () {
+        //     symbol.animate({
+        //         transform: "t-70,40r40"
+        //     }, 100);
+        // }, 400);
+    }
+
+    // function open() {
+    //             clearTimeout(timer);
+
+    //             head.animate({ 
+    //                 transform: "r" + [0, pivots[0]]
+    //             }, 700, mina.elastic);
+                
+    //             jaw.animate({
+    //                 transform: "r" + [0, pivots[1]]
+    //             }, 700, mina.elastic);
+
+    //             symbol.animate({
+    //                 transform: "t0,0r0"
+    //             }, 500, mina.elastic);
+    //         }
+
+    //======================
+    // animation area
+    //======================
+    
+    timer = setTimeout(close, 50);
+    // timer = setTimeout(open, 50);
+
+    //now recursive each hover
+    blackpaw.hover(open, 
+                function () {
+                    timer = setTimeout(close, 200);
+                }
+            );
+
+}
